6bb3836b511abefc5e9060f1be2f6d1d68710a3b
Trastlian <627843792@qq.com>
2018-12-03 14:55:43 +0800

    'test'

---
+++ database/migrations/2018_12_03_052131_create_history_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateHistoryTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('history', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('good_id');
+            $table->double('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('history');
+    }
+}
